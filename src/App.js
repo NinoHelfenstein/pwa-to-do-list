c30aa48069e11d71a679bf52546e41fc747b644e
Nino Helfenstein <nino.helfenstein@hispeed.ch>
2023-07-25 16:00:14 +0200

    revert and go back to working version

--- src/App.js
+++ src/App.js
@@ -6,11 +6,23 @@ export default function App() {
     { id: 2, todo: "Example Todo Nr 2", done: false },
     { id: 3, todo: "Example Todo Nr 3", done: true },
   ]);
-
+  async function giphyTest() {
+    console.log("process.env:", process.env);
+    console.log(
+      "process.env.REACT_APP_GIPHY_API_KEY",
+      process.env.REACT_APP_GIPHY_API_KEY
+    );
+    console.log(
+      await fetch(
+        "https://api.giphy.com/v1/gifs/random?api_key=KKNdzmabIM8dSsWIARkst0tmOJVg1oXf&tag=relax&rating=g"
+      )
+    );
+  }
   useEffect(() => {
     const length = todos.filter((todo) => todo.done === false).length;
     if (length === 0) {
       document.title = "You got nothing to do, relax 🍃📚";
+      giphyTest();
     } else {
       document.title = `${
         todos.filter((todo) => todo.done === false).length
@@ -27,6 +39,7 @@ export default function App() {
     );
     setTodos(updatedTodos);
   };
+
   const removeToDo = (todoId) => {
     // Filter out the todo with the given id
     const updatedTodos = todos.filter((todo) => todo.id !== todoId);
